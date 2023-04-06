3e4d5513a34bf24c7876ace38e43e1ffbe471c0c
Limont <1261750677@qq.com>
2023-04-06 16:28:14 +0800

    style(): 主题样式更改

--- src/views/task/faultRepair/toRepair.vue
+++ src/views/task/faultRepair/toRepair.vue
@@ -4,7 +4,7 @@
       <div class="pages-fault-toRepair-card-title">Info</div>
       <el-form class="pages-fault-toRepair-card-form" :model="base" :rules="rule" ref="ruleForm">
         <el-form-item prop="title" label="Fault Title">
-          <el-input v-model="base.title" type="text" />
+          <el-input maxlength="50" v-model="base.title" type="text" />
         </el-form-item>
         <el-form-item prop="remark" label="Fault Description">
           <el-input maxlength="200" v-model="base.remark" type="textarea" />
